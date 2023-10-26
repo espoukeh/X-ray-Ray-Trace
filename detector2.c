c3a10efd112f50ed8805ddd7ed82d1e887a3eabb
espoukeh <espoukeh@uwm.edu>
2023-10-26 12:16:06 -0500

    detector2 module - removing ray behind the detector and the rays moving away from detector.

--- detector2.c
+++ detector2.c
@@ -201,7 +201,7 @@ int main(int argc, char *argv[])
                 //if (E >= 6 && E <= 9) {
 		if (E >= Energy_min && E <= Energy_max) {
 
-			//fprintf(stderr,"E = %g\n",E);  TEST
+			//fprintf(stderr,"E = %g\n",E);
                         Vector point;
                         myRay.v = unit(myRay.v); //check that direction is a unit vector
 
